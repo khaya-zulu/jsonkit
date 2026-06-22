86c14920fade4ced519c17396d39dbbf1b8bdc66
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2026-06-22 22:28:22 +0200

    update readme

--- apps/client/src/routes/index.tsx
+++ apps/client/src/routes/index.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 
 import {
   AdjustmentsHorizontalIcon,
-  ArrowsPointingOutIcon,
   ArrowUpIcon,
-  Bars3Icon,
 } from "@heroicons/react/16/solid";
-import { ClipboardIcon, ClockIcon } from "@heroicons/react/24/outline";
+import {
+  ClipboardIcon,
+  CodeBracketSquareIcon,
+  FolderIcon,
+  SparklesIcon,
+} from "@heroicons/react/24/outline";
 import { createFileRoute } from "@tanstack/react-router";
 
 import CodeMirror from "@uiw/react-codemirror";
@@ -306,45 +309,33 @@ function RouteComponent() {
 
   return (
     <div className="flex p-4 h-screen bg-gradient-to-b from-stone-50/40 to-violet-50/80 gap-4">
-      <div className="rounded-xl flex-1 ring-1 ring-stone-200/40 shadow-lg p-8 flex bg-white justify-between">
-        <div>
-          <Bars3Icon className="size-5 text-violet-950" />
-        </div>
-
-        <div className="max-w-3xl flex flex-col justify-between w-full">
-          <div className="flex-1 flex flex-col gap-4 px-8 overflow-y-auto hide-scrollbar">
-            {messages.map((message, index) => (
-              <ChatMessage
-                key={index}
-                message={message}
-                createdAt={new Date()}
-              />
-            ))}
+      <div className="rounded-xl flex-1 ring-1 ring-stone-200/40 shadow-lg flex flex-col bg-white">
+        <div className="border-b border-stone-100 flex justify-between items-center">
+          <div className="px-8 py-3 border-r border-stone-100 flex-1">
+            <input
+              className="outline-none placeholder:text-stone-400"
+              placeholder="jqlang query"
+            />
           </div>
-          <div className="p-2 rounded-t-xl mb-2 ring-1 ring-zinc-100/50 text-zinc-600 shadow-sm mx-4 translate-y-2 flex items-center">
-            <div className="flex-1 overflow-auto">
-              <JsonInput value={jsonInputVal} />
-            </div>
-            <div className="px-2.5">
-              <ArrowsPointingOutIcon className="size-5 opacity-40" />
-            </div>
+          <div className="pr-8 pl-4 py-3 flex gap-4 items-center">
+            <SparklesIcon className="size-5" />
+            <CodeBracketSquareIcon className="size-6" />
+            <FolderIcon className="size-5" />
+            <ClipboardIcon className="size-5" />
           </div>
-          <InputBox onSubmit={handleSubmit} />
         </div>
-
-        <div />
+        <div className="h-12 border-b border-stone-100"></div>
+        <div className="flex-1 overflow-auto px-4 py-2 hide-scrollbar overscroll-contain">
+          <JsonInput value={jsonInputVal} isFormatted />
+        </div>
+        <div className="px-8 py-3.5 border-t border-stone-100 text-stone-500">
+          Temporary storage: tmp/test.json
+        </div>
       </div>
 
-      <div className="rounded-xl w-[30rem] shadow-lg ring-1 ring-stone-200/40 bg-white overflow-hidden flex flex-col">
-        <div className="p-4 bg-white flex justify-end border-b border-stone-100">
-          <ClipboardIcon className="size-5" />
-        </div>
-        <div className="relative flex-1 overflow-hidden px-4 pb-4">
-          <div className="overflow-auto h-full hide-scrollbar overscroll-contain py-6">
-            <JsonInput value={jsonInputVal} isFormatted />
-          </div>
-          <div className="h-10 rounded-b-md w-full absolute bottom-0 left-0 bg-gradient-to-b from-stone-50 to-white border-t border-stone-100" />
-        </div>
+      <div className="rounded-xl w-[35rem] shadow-lg ring-1 ring-stone-200/40 p-4 bg-white overflow-hidden flex flex-col">
+        <div className="flex-1"></div>
+        <InputBox onSubmit={handleSubmit} />
       </div>
     </div>
   );
